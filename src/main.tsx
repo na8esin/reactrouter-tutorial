0fbb0f6f76ca2edcccbd4c9f3c8aaf922b28d028
sikmi_watanabe <na8esin.mpst@gmail.com>
2024-07-22 15:13:56 +0900

    Render an <Outlet>

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,23 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+
 import './index.css'
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
