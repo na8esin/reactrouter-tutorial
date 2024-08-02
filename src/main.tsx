0301bb46990211afcdcb2f062222f03f9cb3fd62
Takayuki Watanabe <watanabe@sikmi.com>
2024-08-02 18:28:13 +0900

    edit.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import ErrorPage from "./error-page";
 import Contact, {
   loader as contactLoader,
 } from "./routes/contact";
+import EditContact from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],
